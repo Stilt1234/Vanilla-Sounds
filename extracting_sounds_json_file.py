b5ccf00f516b20e7c82a7ae400e2b99bfe99b691
Stilt1234 <abhilashghosh2011@gmail.com>
2026-02-15 00:44:08 +0530

    Updated Code

--- extracting_sounds_json_file.py
+++ extracting_sounds_json_file.py
@@ -14,11 +14,9 @@ def extract_sounds_json_file():
                 if(key == "minecraft/sounds.json"):
                     hash = value["hash"]
 
-        os.mkdir(os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "for_resourcepack_creators_sounds.json"))
         shutil.copy(os.path.join(os.getcwd(), "minecraft", "assets", "objects", f"{hash[:2]}", f"{hash}"), os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "for_resourcepack_creators_sounds.json"))
         shutil.copy(os.path.join(os.getcwd(), "minecraft", "assets", "objects", f"{hash[:2]}", f"{hash}"), os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "minecraft"))
         os.rename(os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "for_resourcepack_creators_sounds.json", f"{hash}"), os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "for_resourcepack_creators_sounds.json", "sounds.json"))
         os.rename(os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "minecraft", f"{hash}"), os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "minecraft", "sounds.json"))
     except:
         print("No sounds.json file found as it is a version before 1.7.2 therefore skipping extracting sounds.json file.")
-        shutil.rmtree(os.path.join(os.getcwd(), "Vanilla Sounds", "assets", "for_resourcepack_creators_sounds.json"))
